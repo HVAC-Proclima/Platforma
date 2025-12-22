9ce620ccb63d2468ba92f9252aa9b043fe69fb11
HVAC-Proclima <dragos.varvara@proclima.ro>
2025-12-22 03:15:32 +0200

    api.js fixed - v2

--- backend/frontend/src/api.js
+++ backend/frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080"
+const API_URL = "https://platforma-production-4ff1.up.railway.app";
 
 function getToken() {
   return localStorage.getItem("token")
